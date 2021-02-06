3baf3351ec6a2830aaa61dbdd46e6ecccce811b9
SharadRawat <rawat.33@osu.edu>
2021-02-06 16:44:37 +0000

    Add motor_control package

---
+++ src/motor_driver/src/motor_driver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+def sample_publish():
+	pub = rospy.Publisher('SampleMsg', String, queue_size=10)
+	rospy.init_node('motor_driver', anonymous= True)
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		string_msg = "Hello there %s" % rospy.get_time()
+		rospy.loginfo(string_msg)
+		pub.publish(string_msg)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		sample_publish()
+	except rospy.ROSInterruptException:
+		pass 
