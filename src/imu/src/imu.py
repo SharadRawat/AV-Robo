cbeb306ea23b5563fb95d725d1f1fc59c6246338
SharadRawat <rawat.33@osu.edu>
2021-02-18 07:43:45 +0100

    Make IMU rosnode run

--- src/imu/src/imu.py
+++ src/imu/src/imu.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python2
+
 import RTIMU
-from std_msgs import String
+import rospy
+import sys
+from std_msgs.msg import String
 
 def main(args):
 	rospy.init_node('imu')
 	imu_pub = rospy.Publisher('imu', String, queue_size=10)
-
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		ola_cabs = "Printing rubbish at %s" % rospy.get_time()
+		rospy.loginfo(ola_cabs)
+		imu_pub.publish(ola_cabs)
+		rate.sleep()
+		
 if __name__ == "__main__":
 	try:
 		main(sys.argv)
