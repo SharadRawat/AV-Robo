f74199bb3f19833503c9d0ff96ca38235bf77875
SharadRawat <rawat.33@osu.edu>
2021-02-20 10:02:42 +0100

    Make Ultrasonic sensor ros package work

--- src/ultrasonic_sensor/src/read_ultrasonic_sensor.py
+++ src/ultrasonic_sensor/src/read_ultrasonic_sensor.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python2
 
-import RTIMU
 import rospy
 import sys
+import serial
+import statistics
 from std_msgs.msg import Float32
+
+ser = serial.Serial('/dev/ttyUSB0',9600)
+
+
+def distance():
+    s = [0,1]
+    read_serial=ser.readline().strip()
+    values = read_serial.decode('ascii').split(',')
+    a = [float(s) for s in values]
+    return a
 
 def main(args):
 	rospy.init_node('ultrasonic_sensor')
-	imu_pub = rospy.Publisher('/ultrasonic_sensor_msg/double', Float32, queue_size=10)
+	imu_pub = rospy.Publisher('/ultrasonic_sensor_msg', Float32, queue_size=10)
 	rate = rospy.Rate(10)
+	dist = []
+	count = 0
 	while not rospy.is_shutdown():
-		ola_cabs = rospy.get_time()
-		rospy.loginfo(ola_cabs)
-		imu_pub.publish(ola_cabs)
-		rate.sleep()
+	    if count >= 20:
+		dist = dist[len(dist)-10:]
+	    dist += distance()
+	    distance_msg = statistics.mean(dist)
+	    rospy.loginfo(distance_msg)
+	    imu_pub.publish(distance_msg)
+	    count +=1
+	    rate.sleep()
 		
 if __name__ == "__main__":
 	try:
