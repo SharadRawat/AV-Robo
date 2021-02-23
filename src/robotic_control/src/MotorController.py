0bccb5ff8730a5bfb61cfaa14b706685cb1dd9ce
Sharad Rawat <Sharad.Rawat@partner.bmw.de>
2021-02-23 08:01:13 +0100

    Add a control algorithm for motor control

--- src/robotic_control/src/MotorController.py
+++ src/robotic_control/src/MotorController.py
@@ -3,19 +3,40 @@
 import numpy as np
 
 class MotorController(object):
-	
+
 	def __init__(self, max_speed, max_omega):
-		
+
 		# These params are to be tuned.
-		
+
 		self.kp = 3
 		self.ka = 8
 		self.kb = 0
-		
+
 		self.max_speed = max_speed
 		self.max_omega = max_omega
-		
-	#def compute_vel(self, state, goal):
-		
-	#	return vel, omega, is_at_goal
-		
+
+	def compute_vel(self, state, goal):
+
+		delx = goal[0] - state[0]
+		dely = goal[1] - state[1]
+		theta = state[2]
+
+		p = np.sqrt((delx ** 2) + (dely ** 2))
+		alpha = -theta + np.arctan2(dely, delx)
+		beta = -theta - alpha
+
+		v = p * self.kp
+		omega = (alpha * self.ka) + (beta * self.kb)
+		if np.all(v > self.max_speed):
+			v = self.max_speed
+
+		if np.all(omega > self.max_omega):
+			omega = self.max_omega
+
+		if np.all(p < 0.15):
+			done = True
+		else:
+			done = False
+
+		vw = (v, omega, done)
+		return vw
--- src/robotic_control/src/RoboticControl.py
+++ src/robotic_control/src/RoboticControl.py
@@ -11,47 +11,63 @@ from MotorController import MotorController
 class RoboticControl:
 	def __init__(self, t_cam2body):
 		self.ros_interface = ROSInterface(t_cam2body)
-		
+
 		self.time_init = -1.0
-		max_speed = None
-		max_omega = None
+		max_speed = 0.3 // Param
+		max_omega = 1.6 // Param
 		self.motor_controller = MotorController(max_speed, max_omega)
-		
+
+	def switch_to_state(self, state):
+        self.current_state = state
+
 	def process_measurements(self):
 		cam_measurements = self.ros_interface.get_cam_measurements()
 		imu_measurements = self.ros_interface.get_imu()
-		
-		if self.time_init == -1.0:
-			self.time_init = rospy.get_time()
-		self.time_measurement = rospy.get_time()
-		diff_time = (self.time_measurement - self.time_init)
-		
-		if diff_time >=1:
-			self.ros_interface.command_velocity(1, 10)
+
+		if cam_measurements != None:
+    		print("Cam measurements: ", cam_measurements)
+			state = np.array([0.0, 0.0, 0.0])
+            goal = np.array([meas[0], -meas[1], meas[2]])
+
+			vw = self.diff_drive_controller.compute_vel(state, goal)
+			print("Computed command vel: ", vw)
+
+			if vw[2] == False:
+    			self.ros_interface.command_velocity(vw[0], vw[1])
+
+			else:
+    			self.ros_interface.command_velocity(0, 0)
+
+			return
+
 		else:
-			self.ros_interface.command_velocity(-5, 3)				
+
+			print("No Measurement.")
+			return
+
+
 
 if __name__ == '__main__':
 	try:
 		rospy.init_node('robotic_control')
-		
+
 		param_path = rospy.get_param("~param_path")
 		f = open(param_path, 'r')
 		params_raw = f.read()
 		f.close()
-		
+
 		params = yaml.load(params_raw)
 		t_cam2body = params['t_cam2body']
-		
+
 		robotic_control = RoboticControl(t_cam2body)
-		
+
 		rate_of_measurement = rospy.Rate(60)
 		while not rospy.is_shutdown():
 			robotic_control.process_measurements()
 			rate_of_measurement.sleep()
-		
+
 		## Once Done, stop the robot
 		robotic_control.ros_interface.command_velocity(0, 0)
 
 	except rospy.ROSInterruptException:
-		pass 
+		pass
